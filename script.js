0bc240ed54c2bc9080ba28207f3450c173edfcd3
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-07-10 22:53:21 +0300

    btn style + theme functionality

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const lightThemeIcon = document.getElementById('light-theme-icon');
+const darkThemeIcon = document.getElementById('dark-theme-icon');
+
+let themeToggle = document.getElementById('theme-toggle');
+let storedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+if (storedTheme) {
+    document.documentElement.setAttribute('data-theme', storedTheme)
+    if (storedTheme === 'dark') {
+        lightThemeIcon.classList.add('hide');
+        darkThemeIcon.classList.remove('hide');
+    }
+    else {
+        lightThemeIcon.classList.remove('hide');
+        darkThemeIcon.classList.add('hide');
+    }
+}
+themeToggle.addEventListener('click', function () {
+    let currentTheme = document.documentElement.getAttribute('data-theme');
+    let targetTheme = 'light';
+    if (currentTheme === 'light') {
+        targetTheme = 'dark';
+    }
+    lightThemeIcon.classList.toggle('hide');
+    darkThemeIcon.classList.toggle('hide');
+    document.documentElement.setAttribute('data-theme', targetTheme);
+    localStorage.setItem('theme', targetTheme);
+});
+// theme switch
+
 function setClock(digits, arr) {
     digits.forEach((digit, dIndex) => {
         let options = digit.children;
